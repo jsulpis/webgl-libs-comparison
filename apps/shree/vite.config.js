a219e10ba7aa48f4f5dc2597e8f1f445866e587e
Julien Sulpis <julien.sulpis@gmail.com>
2023-08-09 22:02:16 +0200

    chore: setup CI and deploy to GitHub Pages

--- apps/shree/vite.config.js
+++ apps/shree/vite.config.js
@@ -2,5 +2,9 @@ import glsl from 'vite-plugin-glsl';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/webgl-tools-comparison/shree/',
+  build: {
+    outDir: '../../public/shree',
+  },
   plugins: [glsl()]
 });
