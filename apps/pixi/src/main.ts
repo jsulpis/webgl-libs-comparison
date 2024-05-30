7249f629f93f8ddfa1ddb587b38d692a5538e281
Julien Sulpis <julien.sulpis@gmail.com>
2024-05-30 19:46:36 +0200

    feat(pixi): implement blob demo

--- apps/pixi/src/main.ts
+++ apps/pixi/src/main.ts
@@ -1,41 +1,53 @@
-import { vertexShader, fragmentShader } from "common";
-
 import * as PIXI from "pixi.js";
+import { vertex, fragment, setup, onCanvasResize } from "common/src";
+import "common/src/styles.css";
 
 function flatten<T>(array: T[][]): T[] {
-   return array.reduce((acc, val) => acc.concat(val), []);
+  return array.reduce((acc, val) => acc.concat(val), []);
 }
 
-const app = new PIXI.Application({ resizeTo: window });
+const canvas = document.querySelector("canvas")!;
 
-// @ts-ignore
-document.body.appendChild(app.view);
+const app = new PIXI.Application({ view: canvas });
 
-// Build geometry.
 const geometry = new PIXI.Geometry()
-   .addAttribute(
-      "position", // the attribute name
-      flatten([
-         [0, 0, 0],
-         [1, 0, 0],
-         [1, 1, 0],
-         [0, 1, 0],
-      ]),
-      3 // the size of the attribute
-   )
-   .addIndex([0, 1, 2, 0, 2, 3]);
+  .addAttribute(
+    "aPosition", // the attribute name
+    flatten([
+      [0, 0],
+      [1, 0],
+      [1, 1],
+      [0, 1],
+    ]),
+    2 // the size of the attribute
+  )
+  .addIndex([0, 1, 2, 0, 2, 3]);
 
 const uniforms = {
-   uTime: 0,
+  uTime: 0,
+  uMouse: [0, 0],
+  uResolution: [0, 0],
 };
 
-// Build the shader and the quad.
-const shader = PIXI.Shader.from(vertexShader, fragmentShader, uniforms);
+const shader = PIXI.Shader.from(vertex, fragment, uniforms);
 const quad = new PIXI.Mesh(geometry, shader);
 
 app.stage.addChild(quad);
 
-// start the animation..
 app.ticker.add(() => {
-   quad.shader.uniforms.uTime = performance.now() / 500;
+  quad.shader.uniforms.uTime = performance.now() / 500;
+});
+
+const { onMouseUpdate } = setup(canvas);
+onMouseUpdate((x, y) => {
+  const currentMouse = quad.shader.uniforms.uMouse;
+  quad.shader.uniforms.uMouse = [
+    currentMouse[0] + (x - currentMouse[0]) * 0.05,
+    currentMouse[1] + (y - currentMouse[1]) * 0.05,
+  ];
+});
+
+onCanvasResize(canvas, ({ devicePixelSize }) => {
+  quad.shader.uniforms.uResolution = [devicePixelSize.width, devicePixelSize.height];
+  app.renderer.resize(devicePixelSize.width, devicePixelSize.height);
 });
